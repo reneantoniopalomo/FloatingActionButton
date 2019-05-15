a48e4b58d3cedd3928e6b1912037d627910930f4
reneantoniopalomo <48412939+reneantoniopalomo@users.noreply.github.com>
2019-05-15 17:28:41 -0500

    Rotar floating action button al clickearlo

--- app/src/main/java/com/example/floatingactionbutton/MainActivity1.java
+++ app/src/main/java/com/example/floatingactionbutton/MainActivity1.java
@@ -1,66 +1,45 @@
 package com.example.floatingactionbutton;
-
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
-public class MainActivity1 extends AppCompatActivity {
+public class MainActivity1 extends AppCompatActivity implements View.OnClickListener {
     private FABToolbarLayout morph;
-    private FABToolbarLayout layout;
-    private View one, two, three, four;
-    private ListView list;
-    private View fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
-        layout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
-        one = findViewById(R.id.one);
-        two = findViewById(R.id.two);
-        three = findViewById(R.id.three);
-        four = findViewById(R.id.four);
-        list = (ListView) findViewById(R.id.list);
-        fab = findViewById(R.id.fabtoolbar_fab);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        one.setOnClickListener((View.OnClickListener) this);
-        two.setOnClickListener((View.OnClickListener) this);
-        three.setOnClickListener((View.OnClickListener) this);
-        four.setOnClickListener((View.OnClickListener) this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        morph = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
 
-        String[] data = new String[30];
-        for (int i = 0; i < 30; i++) {
-            data[i] = "Test " + i;
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                layout.show();
-            }
-        });
+        View uno, dos, tres, cuatro;
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                layout.show();
-            }
-        });
+        uno = findViewById(R.id.uno);
+        dos = findViewById(R.id.dos);
+        cuatro = findViewById(R.id.cuatro);
+        tres = findViewById(R.id.tres);
+
+        fab.setOnClickListener((View.OnClickListener) this);
+        uno.setOnClickListener((View.OnClickListener) this);
+        dos.setOnClickListener((View.OnClickListener) this);
+        tres.setOnClickListener((View.OnClickListener) this);
+        cuatro.setOnClickListener((View.OnClickListener) this);
     }
+
 
     @Override
-    public void onBackPressed() {
-        layout.hide();
-    }
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab) {
+            morph.show();
+        }
 
+        morph.hide();
+    }
 }
